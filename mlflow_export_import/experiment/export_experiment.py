5757873a8155fd716a308667efae0e0311edfb9a
sadra.mirhendi <s.s.mirhendi@gmail.com>
2022-03-15 12:19:30 -0230

    export permissions

--- mlflow_export_import/experiment/export_experiment.py
+++ mlflow_export_import/experiment/export_experiment.py
@@ -79,7 +79,8 @@ class ExperimentExporter():
             failed_run_ids.append(run.info.run_id)
 
     def _export_permissions(self, src_exp_id, fs, output_dir):
-        permissions_data = self.dbx_client.get("preview/permissions/experiments/{}".format(src_exp_id))
+        print(src_exp_id)
+        permissions_data = self.dbx_client.get("preview/permissions/experiments/{}/permissionLevels".format(src_exp_id))
         path = os.path.join(output_dir, "permissions.json")
         utils.write_json_file(fs, path, permissions_data)
 
