651f3c938c4c88502dd4f1a615b22228b2e88f44
sadra.mirhendi <s.s.mirhendi@gmail.com>
2022-03-28 12:22:33 -0230

    Fix json write

--- mlflow_export_import/bulk/import_models.py
+++ mlflow_export_import/bulk/import_models.py
@@ -76,7 +76,7 @@ def import_all(input_dir, delete_model, use_src_user_id, import_metadata_tags, v
     duration = round(time.time() - start_time, 1)
     dct = { "duration": duration, "experiment_import": exp_res[1], "model_import": model_res }
     fs = _filesystem.get_filesystem(".")
-    utils.write_json_file(fs, "import_report.json", dct)
+    utils.write_json_file(fs, "import_report.json", json.dumps(dct))
     print("\nImport report:")
     print(json.dumps(dct,indent=2)+"\n")
 
